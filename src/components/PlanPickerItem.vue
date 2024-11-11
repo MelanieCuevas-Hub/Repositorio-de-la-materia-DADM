37e705344fb78861b4ea69906e1857963e07a7fc
MelanieCuevas-Hub <L211130202@gamadero.tecnm.mx>
2024-11-11 18:51:25 +0000

    🚧S03S05 Eventos Add events to child component

--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,21 +1,34 @@
 <template>
-    <div class="plan">
+    <div @click="selectPlan" class="plan">
       <div class="description">
         <span class="title">
-          {{ name }}
+          {{ name }} {{ selected ? '🌟' : ''}}
         </span>
       </div>
     </div>
   </template>
   
-  <script setup>
+  <script setup> 
+// IMPORT ref FUNCTION
+import { ref } from 'vue';
+
   // Usando macro para definir las props
-  defineProps({
+  const props = defineProps({
     name: {
       type: String,
       required: true
     }
   });
+  // DEFINE EVENT
+  const emit = defineEmits(['select']);
+  // VARIABLE QUE PERMITE RECORDAR SI EL PLAN A SIDO SELECCIONADO
+  const selected = ref(false);
+  //FUNCION QUE PERMITE CAMBIAR EL ESTADO DE LA VARIABLE
+  const selectPlan = () => {
+  selected.value = true;
+  //EMITE EVENTO DE QUE EL PLAN HA SIDO SELECCIONADO
+  emit('select', props.name);
+};
   </script>
   
   <style scoped>
