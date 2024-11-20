8078da471f877431dd2cc88a08decb58033a9176
MelanieCuevas-Hub <L211130202@gamadero.tecnm.mx>
2024-11-20 18:51:21 +0000

    🚧S03S06 Hooks

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
  import PlanPickerItem from './PlanPickerItem.vue';
- import { ref } from 'vue';
+ import { ref, onMounted} from 'vue';
  //Creando una referencia reactiva
  //para almacenar el plan seleccionado
  const selectedPlan = ref(null)
@@ -10,6 +10,14 @@
   //Almacenar el plan seleccionado
 selectedPlan.value = name;
    };
+  //INTENTANDO OBTENER LA REFERCIA AL ELEMENTO PLANS
+  console.log("Termina de ejecutar el script setup 🥳");
+
+  //Registrando el hook mounted
+  onMounted(() => {
+    console.log("El componente PlanPicker se ha montado 🎉")
+    console.log(document.querySelector('.plans'));
+  })
 </script>
 
 <template>
