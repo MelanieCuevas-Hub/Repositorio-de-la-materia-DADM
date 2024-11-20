4130c728063b5f91ef822883ee991081b8140522
MelanieCuevas-Hub <L211130202@gamadero.tecnm.mx>
2024-11-20 17:50:01 +0000

    🌟SO3SO5 Eventos

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,26 +1,29 @@
 <script setup>
  import PlanPickerItem from './PlanPickerItem.vue';
  import { ref } from 'vue';
- // CREANDO UNA REFERENCIA REACTIVA PARA ALAMQCENAR EL PLAN SELECCIONADO
- const selectedPlan =  ref(null)
- const plans =
-  ref(['El cafetero','El chelero','El intelectual']);
+ //Creando una referencia reactiva
+ //para almacenar el plan seleccionado
+ const selectedPlan = ref(null)
+ const plans = 
+ ref(['El cafetero','El chelero','El intelectual']);
  const selectPlan = (name) => {
-// ALMACENAR EL PLAN SELECCIONADO 
- selectedPlan.value = name;
-  }
+  //Almacenar el plan seleccionado
+selectedPlan.value = name;
+   };
 </script>
 
 <template>
 <div class="plans">
-  <PlanPickerItem v-for="plan in plans" :key="plan" :name="plan" @select="selectPlan"
-  :select-plan="selectedPlan"
-  />
+  <PlanPickerItem
+   v-for="plan in plans" 
+   :key="plan"
+    :name="plan"
+    @select="selectPlan"
+    :selected-plan="selectedPlan" />
+
 </div>
-<p> {{ selectedPlan }} </p>
+
 </template>
-
-
 
 <style scoped>
 
--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,40 +1,45 @@
 <template>
-    <div @click="selectPlan" class="plan">
-      <div class="description">
-        <span class="title">
-          {{ name }} {{ isSelected ? '🌟' : ''}}
-        </span>
-      </div>
+  <div @click="selectedPlan"
+   class="plan"
+   :class="{ selected: isSelected}">
+    <div class="description">
+      <span class="title">
+         {{ name }} {{ isSelected ? '🌟' : '' }}  </span>
     </div>
-  </template>
-  
-  <script setup>
-  // IMPORTAMOS UN MACRO
-  import { computed } from 'vue';
+  </div>
+</template>
 
-  // Usando macro para definir las props
-  const props = defineProps({
-    name: {
-      type: String,
-      required: true
-    },
-    selectPlan: String
-  });
-  // DEFINE EVENT
-  const emit = defineEmits(['select']);
-  //FUNCION QUE PERMITE CAMBIAR EL ESTADO DE LA VARIABLE
-  const selectPlan = () => {
-  //EMITE EVENTO DE QUE EL PLAN HA SIDO SELECCIONADO
-  emit('select', props.name);
+<script setup>
+//Importamos la funcion para crear
+//propiedades computada 
+import { computed } from 'vue';
+// Usando macro para definir las props
+// definir las props
+//define props (['name', 'planIcon']);
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }, 
+  selectedPlan: String
+  
+ });
+//Define event 
+const emit = defineEmits(['select']); 
+//Función que permite cambiar el estado de la variable
+// selected
+const selectedPlan = () => {
+   //Emitir que el evento ha sido seleccionado
+  //plan ha sido seleccionado
+emit('select', props.name);
 };
-//CREANDO UNA PROPIEDAD COMPUTADA 
-//PARA SABER SI EL ´PLAN A SIDO SELECCIONADO
-const isSelected = computed(() => { 
-   return props.name === props.selectPlan;
+//Creando una propiedad computada 
+//para saber si el plan ha sido seleccionado
+const isSelected = computed (() =>{
+  return props.name === props.selectedPlan;
+});
+</script>
 
-})
-  </script>
-  
-  <style scoped>
-  /* Agrega tus estilos aquí */
-  </style>
+<style scoped>
+/* Agrega tus estilos aquí */
+</style>
